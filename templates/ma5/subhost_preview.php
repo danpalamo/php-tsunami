3b27be23dd623b66daabf8521cf75605a177d3a2
danpalamo <contact@caglan.net>
2017-07-26 16:57:26 -0500

    misc tweaks html template

--- templates/ma5/subhost_preview.php
+++ templates/ma5/subhost_preview.php
@@ -3,18 +3,17 @@
 		<b style='font-size:20px;'><a href="<?php _subhost(); ?>"><?php _subhost(); ?></a></b><br/>
 		<b>IP</b> <a target="blank" href="http://<?php _e('IP', true); ?>"><?php _e('IP', true); ?></a><br/>
 		<b>Site Name</b> <?php _e('SITE_NAME', true); ?><br/>
-		<b>Status</b> <?php echo __e('ASSOCIATION', true) == 1 ? 'Associated' : 'Not Associated'; ?><br/>
-	</td>
-	<td class="center">
-		<?php _g("ASSOCIATION", true, true); ?>
 	</td>
 	<td class="center">
 		<?php _g("SES_UPTIME", true, true); ?>
 	</td>
 	<td class="center">
-		<?php _g("RSSI_C_SU", true, true); ?>
+		<?php _g("RF_TRAFFIC_O", true, true); ?>
 	</td>
 	<td class="center">
-		<?php _g("RF_TRAFFIC_O", true, true); ?>
+		<?php _g("RSSI_SU", true, true); ?>
+	</td>
+	<td class="center">
+		<?php _g("RF_PER", true, true); ?>
 	</td>
 </tr>
