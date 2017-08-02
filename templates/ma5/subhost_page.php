7400bc1199a302ef87381e68a484eacd9167b73c
danpalamo <contact@caglan.net>
2017-08-02 15:37:49 -0500

    whoops cdefa problems

--- templates/ma5/subhost_page.php
+++ templates/ma5/subhost_page.php
@@ -4,10 +4,9 @@
 			<b style='font-size:20px;'><a href=".."><?php _host(); ?></a></b> <span style="font-size:12px;line-height:20px;">(<a target="blank" href="http://<?php _host('m_host'); ?>"><?php _host('m_host'); ?></a>)</span><br/>
 			<b style='font-size:20px;'><a href="."><?php _subhost(); ?></a></b> <span style="font-size:12px;line-height:20px;">(<a target="blank" href="http://<?php _e('IP', true); ?>"><?php _e('IP', true); ?></a>)</span><br/>
 			<b>Site Name</b> <?php _e('SITE_NAME', true); ?><br/>
+			<b>FW ver</b> <?php _e('FW_VER', true); ?><br/>
 			<b>CIR D/U</b> <?php _e('CIR_DOWN', true); ?>/<?php _e('CIR_UP', true); ?><br/>
 			<b>MIR D/U</b> <?php _e('MIR_DOWN', true); ?>/<?php _e('MIR_UP', true); ?><br/>
-			<b>Adapt Rate</b> <?php _e('ADAPT_RATE', true); ?><br>
-			<b>FW ver</b> <?php _e('FW_VER', true); ?><br/>
 		</td>
 		<td class="center">
 			<?php _g("MA5_SES_UPTIME", true, false); ?><br/><hr/>
