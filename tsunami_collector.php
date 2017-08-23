fc3b842d6450f82a6974d8844f33cca885e58104
danpalamo <contact@caglan.net>
2017-08-23 11:29:51 -0500

    testing

--- tsunami_collector.php
+++ tsunami_collector.php
@@ -152,8 +152,9 @@ foreach($aHosts as $hostKey => $host)
 	//if cannot reach uptime snmp query
 	try {
 		$snmpCheck = $getFunction($host->m_host->m_community, ".1.3.6.1.2.1.1.3.0");
+		echo $host->m_host." ".$host->m_community\n";
 		if ($snmpCheck == false) {
-			echo date("Ymd H:i:s")." HOST ".$host->m_desc." not reachable...Skipping.\n";
+			echo date("Ymd H:i:s")." HOST ".$host->m_host." not reachable...Skipping.\n";
 			break;
 		}
 		else {
